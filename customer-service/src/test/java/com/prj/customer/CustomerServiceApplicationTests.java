9709b2682738968fa4d9b89966d6f9cda49b6655
Abdelhak <abdelhak.farsi.externe@chronopost.net>
2025-12-01 14:18:14 +0100

    first project structure

---
+++ customer-service/src/test/java/com/prj/customer/CustomerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prj.customer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ notification-service/src/test/java/com/prj/notification/NotificationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prj.notification;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
